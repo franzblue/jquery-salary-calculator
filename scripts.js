1c4e3b6f715766ba168a291be307f557a8294edf
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2020-10-03 18:19:54 -0500

    added comments outlying next functions needed

--- scripts.js
+++ scripts.js
@@ -15,12 +15,26 @@ function onReady(){
     // functions down here!!!
 
     function gatherData(){
-        let employee = {};
+        let _employee = {};
         employee.fName = $('#firstName').val();
         employee.lName = $('#lastName').val();
         employee.id = $('#idNumber').val();
         employee.title = $('#title').val();
         employee.salary = $('#annualSalary').val();
-        employeeArray.push(employee);
-        console.log(employee);
+        employeeArray.push(_employee);
+        console.log(_employee);
     }
+
+    function tableFill(){
+        
+    }
+    
+
+
+    // append info the DOM and clear inputs
+
+    // calculate monthly costs
+
+    //if exceeds $20k add red background
+
+    // delete button that removes employee from DOM
