9c1afd6e9639dc783511e4d888d82a4132c2ec71
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2020-10-03 08:13:01 -0500

    sourced jquery

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
 console.log('Hello from js');
+
+$(document).ready(onReady);
+
+function onReady(){
+    console.log('jquery good to go');
+
+    // event listeners here!!!
+
+}
+
+    // functions down here!!!
