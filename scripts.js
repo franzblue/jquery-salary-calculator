d1f13b49a2dedb6d5d92624d8a6e5f0e91c5e216
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2020-10-03 11:54:57 -0500

    button click now gathers input data and pushes it as an object into employeeArray

--- scripts.js
+++ scripts.js
@@ -1,17 +1,26 @@
 console.log('Hello from js');
 
+employeeArray = [];
+
 $(document).ready(onReady);
 
 function onReady(){
     console.log('jquery good to go');
 
     // event listeners here!!!
-    $('#submitButton').on('click', buttonWorks);   // not hooked up right
+    $('#submitButton').on('click', gatherData);
 
 }
 
     // functions down here!!!
 
-    function buttonWorks(){
-        console.log('Submit button works!');  // not hooked up right
+    function gatherData(){
+        let employee = {};
+        employee.fName = $('#firstName').val();
+        employee.lName = $('#lastName').val();
+        employee.id = $('#idNumber').val();
+        employee.title = $('#title').val();
+        employee.salary = $('#annualSalary').val();
+        employeeArray.push(employee);
+        console.log(employee);
     }
