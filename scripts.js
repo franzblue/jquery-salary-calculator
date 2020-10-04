9d6490dc4ff3b13f64ad2e2caeea78e648c97883
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2020-10-03 19:04:07 -0500

    I am trying to create functions but am too tired. Commented out all js work since last commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 console.log('Hello from js');
 
-employeeArray = [];
+let employeeArray = [];
+let _employee = {};
+let cost = 0;
 
 $(document).ready(onReady);
 
@@ -9,6 +11,9 @@ function onReady(){
 
     // event listeners here!!!
     $('#submitButton').on('click', gatherData);
+    // $('#submitButton').on('click', tableFill);
+    // $('#submitButton').on('click', monthlyCost);
+
 
 }
 
@@ -16,19 +21,44 @@ function onReady(){
 
     function gatherData(){
         let _employee = {};
-        employee.fName = $('#firstName').val();
-        employee.lName = $('#lastName').val();
-        employee.id = $('#idNumber').val();
-        employee.title = $('#title').val();
-        employee.salary = $('#annualSalary').val();
+        _employee.fName = $('#firstName').val();
+        _employee.lName = $('#lastName').val();
+        _employee.id = $('#idNumber').val();
+        _employee.title = $('#title').val();
+        _employee.salary = $('#annualSalary').val();
         employeeArray.push(_employee);
         console.log(_employee);
     }
 
-    function tableFill(){
-        
-    }
+    // function tableFill(){
+    //     let newRow = $('#tableBody')
+
+    //     newRow.empty();
+
+    //     for( i = 0; i < employeeArray.length; i++){
+    //         newRow.append(`
+    //             <tr>
+    //                 <td>${_employee[i].fName}</td>
+    //                 <td>${_employee[i].lName}</td>
+    //                 <td>${_employee[i].id}</td>
+    //                 <td>${_employee[i].title}</td>
+    //                 <td>${_employee[i].salary}</td>
+    //                 <td><button id="deleteButton">Delete</button></td>
+    //             </tr>
+    //             `);
+    //     }
+    //         console.log('Lets see if this works');
+    // }
     
+    // above function almost works
+
+    // function monthlyCost(){
+    //     cost += $('#annualSalary').val();
+    //     console.log('Monthly cost is ', cost);
+    //     return cost;
+    // }
+
+    //above function is half assed, I need rest
 
 
     // append info the DOM and clear inputs
@@ -38,3 +68,5 @@ function onReady(){
     //if exceeds $20k add red background
 
     // delete button that removes employee from DOM
+
+    // for( i = 0; i < employeeArray.length; i++)
